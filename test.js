7ccd827db080767c0abf719979f8f6f3ab108e68
20130311RLE1\Eigenaar <svanhouten@gmail.com>
2017-01-27 10:31:41 +0100

    no message

---
+++ test.js
@@ -0,0 +1,9 @@
+function greeting (name, color){
+    console.log ("Hello, my name is " + name + " and I like the color " + color )
+
+
+};
+
+var SjoerdName =  "Sjoerd"
+greeting (SjoerdName, "blue");
+greeting ("Jane", "green" )

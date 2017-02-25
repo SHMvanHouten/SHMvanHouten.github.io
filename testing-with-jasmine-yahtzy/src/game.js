7ae42541789ea4e1cc8ec71b25fa710192a6bab9
Sjoerd van Houten <svanhouten@gmail.com>
2017-02-25 23:53:51 +0100

    starting an outline of methods needed

---
+++ testing-with-jasmine-yahtzy/src/game.js
@@ -0,0 +1,11 @@
+function YahtzeeGame(names, visualiser, scoreChecker){
+    var dice = []
+
+}
+YahtzeeGame.prototype.rollDice = function(){
+     dice = [1,2,3,4,5]
+};
+YahtzeeGame.prototype.selectInput = function(){
+    return scoreChecker.getScoreAsFullHouse();
+
+};

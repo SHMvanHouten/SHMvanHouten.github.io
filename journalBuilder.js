34dbe25c01353f45bd2c819e5926c52e40dc96ac
Sjoerd <svanhouten@gmail.com>
2017-04-21 21:13:27 +0200

    moved journalentries to separate folder, changed their names to be more logical and easy to find. Changed listOfJournalEntries to an object to make the JournalBuilder look more logical.

--- journalBuilder.js
+++ journalBuilder.js
@@ -1,24 +1,24 @@
-function EntryGetter(entry){
-    var client = new HttpClient();
-    var entryName = "entry" + entry;
-    var hasArticleBeenOpened = false;
+function EntryGetter(entryName){
+    const client = new HttpClient();
+    let hasArticleBeenOpened = false;
 
     this.getArticle = function(){
         if(hasArticleBeenOpened){
             document.getElementById(entryName).innerHTML = "";
             hasArticleBeenOpened = false;
         }else{
-            client.get('/' + entryName, function(response){
-                entry = document.getElementById(entryName);
+            client.get('/JournalEntries/' + entryName + ".html", function(response){
+                let entry = document.getElementById(entryName);
                 entry.innerHTML = response;
             });
             hasArticleBeenOpened = true;
         }
     }
 }
+
 function HttpClient() {
     this.get = function(aUrl, aCallback) {
-        var anHttpRequest = new XMLHttpRequest();
+        let anHttpRequest = new XMLHttpRequest();
         anHttpRequest.onreadystatechange = function() {
             if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
                 aCallback(anHttpRequest.responseText);
@@ -31,20 +31,39 @@ function HttpClient() {
 
 function JournalBuilder(listOfJournalEntries){
 
-    var populateTheJournal = function(){
-        var main = document.getElementsByTagName("main")[0];
-        for(let i=listOfJournalEntries.length -1; i>=0; i--){
-            var section = document.createElement("section");
-            var h2 = document.createElement("h2");
-            h2.innerHTML = listOfJournalEntries[i];
-            var getEntry = new EntryGetter(i+1);
+    let populateTheJournal = function(){
+        let main = document.getElementsByTagName("main")[0];
+        Object.keys(listOfJournalEntries).forEach(function(key){
+            let section = document.createElement("section");
+            let h2 = document.createElement("h2");
+            h2.innerHTML = listOfJournalEntries[key];
+            let article = document.createElement("article");
+            article.id = key;
+            let getEntry = new EntryGetter(key);
             h2.addEventListener("click",getEntry.getArticle);
             section.appendChild(h2);
-            var article = document.createElement("article");
-            article.id = "entry" + (i + 1);
             section.appendChild(article);
             main.appendChild(section)
-        }
+        });
+
+
+        // for(let i=listOfJournalEntries.length -1; i>=0; i--){
+        //     let section = document.createElement("section");
+        //     let h2 = document.createElement("h2");
+        //     h2.innerHTML = listOfJournalEntries[i];
+        //     let getEntry = new EntryGetter(i+1);
+        //     h2.addEventListener("click",getEntry.getArticle);
+        //     section.appendChild(h2);
+        //     let article = document.createElement("article");
+        //     article.id = "entry" + (i + 1);
+        //     section.appendChild(article);
+        //     main.appendChild(section)
+        // }
     };
     populateTheJournal();
+}
+
+function JournalEntry(name, url){
+    this.name = name;
+    this.url = url;
 }
